b9633c45b1d3453f3a543733d47616c55704afbb
Andrian Pascal <andrian.pascal@stud.tecnicosuperiorekennedy.it>
2021-06-21 13:53:32 +0200

    1.1

--- itsAltoAdriatico/src/app/app-routing.module.ts
+++ itsAltoAdriatico/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"chi-siamo",component:ChiSiamoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- itsAltoAdriatico/src/app/app.module.ts
+++ itsAltoAdriatico/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChiSiamoComponent
   ],
   imports: [
     BrowserModule,
