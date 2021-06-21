6406edf308ad7d49ab37e28ddeb920aeaceba7d1
Nicola Bravi <nicola.bravi@stud.tecnicosuperiorekennedy.it>
2021-06-21 12:47:11 +0200

    Started creating course form

--- itsAltoAdriatico/src/app/app-routing.module.ts
+++ itsAltoAdriatico/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubscriptionCourseComponent } from './subscription-course/subscription-course.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sub-course', component: SubscriptionCourseComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- itsAltoAdriatico/src/app/app.module.ts
+++ itsAltoAdriatico/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+
+import { SubscriptionCourseModule } from './subscription-course/subscription-course.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubscriptionCourseComponent } from './subscription-course/subscription-course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscriptionCourseComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
