f47b08e2d6688cec5152b40db03c8dbf1a10bcb9
Jaguarinho <lucadadda12346@gmail.com>
2021-06-21 17:49:09 +0200

    News list

--- itsAltoAdriatico/src/app/app.module.ts
+++ itsAltoAdriatico/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsListComponent } from './news-list/news-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule,
