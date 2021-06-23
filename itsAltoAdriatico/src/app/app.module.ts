b9b5dcc43725d12bad36cb22c4954a55c2d6117e
Giacomo Curreli <giacomo.curreli@stud.tecnicosuperiorekennedy.it>
2021-06-23 17:14:08 +0200

    open day card

--- itsAltoAdriatico/src/app/app.module.ts
+++ itsAltoAdriatico/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsCardComponent } from './news-card/news-card.component';
+import { OpenDayCardComponent } from './open-day-card/open-day-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsCardComponent,
+    OpenDayCardComponent
   ],
   imports: [
     BrowserModule,
