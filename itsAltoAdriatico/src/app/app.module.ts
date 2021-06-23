c4f21b479bb15b41f7b5d068f93f5a31f9038ad6
Jaguarinho <lucadadda12346@gmail.com>
2021-06-23 17:40:44 +0200

    first commit

--- itsAltoAdriatico/src/app/app.module.ts
+++ itsAltoAdriatico/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsDetailComponent } from './news-detail/news-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsDetailComponent
   ],
   imports: [
     BrowserModule,
