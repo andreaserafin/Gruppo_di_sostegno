20045e4b5b25653da109364d5c317cc6ae8a5cb5
Giacomo Curreli <giacomo.curreli@stud.tecnicosuperiorekennedy.it>
2021-06-23 17:55:20 +0200

    open-day completa

--- itsAltoAdriatico/src/app/app-routing.module.ts
+++ itsAltoAdriatico/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsCardComponent } from './news-card/news-card.component';
+import { OpenDayCardComponent } from './open-day-card/open-day-card.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path:'openday', component: OpenDayCardComponent
+},
+{
+  path:'newscard', component: NewsCardComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
