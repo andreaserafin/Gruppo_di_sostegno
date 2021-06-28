4bf2f90dde2be471725eb1b96c85366b0ec81fb3
andreaserafin <andrea.serafin@stud.tecnicosuperiorekennedy.it>
2021-06-28 12:07:29 +0200

    Edited
    footer margin, edited navbar links

--- itsAltoAdriatico/src/app/app-routing.module.ts
+++ itsAltoAdriatico/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
+import { ContattiComponent } from './contatti/contatti.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardCorsoComponent } from './card-corso/card-corso.component';
 import { ListaCorsiComponent } from './lista-corsi/lista-corsi.component';
-
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
 const routes: Routes = [
   {
     path: 'lista-corsi',
     component:  ListaCorsiComponent,
+    pathMatch: 'full',
+  },
+  {
+    path:"chi-siamo",
+    component:ChiSiamoComponent,
+    pathMatch: 'full',},
+  {
+    path:"contatti",
+    component:ContattiComponent,
     pathMatch: 'full',
   }
 ];
