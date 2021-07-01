9da37156a8add2752602fdf50de7fbff9ca5d3d4
Giacomo Curreli <giacomo.curreli@stud.tecnicosuperiorekennedy.it>
2021-07-01 15:32:51 +0200

    con backend

--- itsAltoAdriatico/src/app/lista-corsi/lista-corsi.component.ts
+++ itsAltoAdriatico/src/app/lista-corsi/lista-corsi.component.ts
@@ -1,20 +1,23 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { CardCorsoComponent } from '../card-corso/card-corso.component';
-
+import { CoursePreview } from '../models/data-model';
+import { ProxyApiService } from '../Services/proxy-api.service';
 
 @Component({
   selector: 'app-lista-corsi',
   templateUrl: './lista-corsi.component.html',
-  styleUrls: ['./lista-corsi.component.scss']
+  styleUrls: ['./lista-corsi.component.scss'],
 })
 export class ListaCorsiComponent implements OnInit {
-constructor() { 
-  }
+  constructor(private proxy: ProxyApiService) {}
+
+  corsi: CoursePreview[] = [];
 
   ngOnInit(): void {
-
-    
-
+    this.proxy.getCoursePreview().subscribe((data) => {
+      this.corsi = data;
+      console.log(this.corsi);
+    });
   }
-
 }
